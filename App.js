c311ed3d56bb5f045029f3a17878a0347a5bcec8
Adeeb Rashid <adeeb.rashid@ufl.edu>
2022-11-02 14:57:15 -0400

    Include mapView with starting location at UF
    unable to get location working on ios  expogo

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location'
+
 
 export default function App() {
+  // const [location, setLocation]  = useState(null);
+  // const [errorMsg, setErrorMsg] = useState(null);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     let {status} = await Location.requestBackgroundPermissionsAsync();
+  //     if(status !== 'granted') {
+  //       setErrorMsg('location permissoins denied')
+  //       return;
+  //     }
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //   })();
+  // }, []);
+
+  // let text = 'Waiting...';
+  // if(errorMsg){
+  //   text = errorMsg;
+  // }
+  // else if (location){
+  //   text = JSON.stringify(location);
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Locality</Text>
+      {/* <Text> {text}</Text> */}
       <StatusBar style="auto" />
+      <MapView style={styles.map} initialRegion={{
+      latitude: 29.643633,
+      longitude: -82.354927,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}/>
     </View>
   );
 }
@@ -16,5 +50,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 2,
+    // ...StyleSheet.absoluteFillObject,
   },
 });

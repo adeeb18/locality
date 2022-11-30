3868d23caee93e268647bfeba6b904e4da9dfa53
tic-Nimmons <n.timmons@ufl.edu>
2022-11-30 18:08:10 -0500

    Added pins to map with descriptions

--- App.js
+++ App.js
@@ -7,170 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from './firebase';
+import {Marker} from 'react-native-maps';
 
-const Map = () => {
-  return(
-  // const [location, setLocation]  = useState(null);
-  // const [errorMsg, setErrorMsg] = useState(null);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     let {status} = await Location.requestBackgroundPermissionsAsync();
-  //     if(status !== 'granted') {
-  //       setErrorMsg('location permissoins denied')
-  //       return;
-  //     }
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     setLocation(location);
-  //   })();
-  // }, []);
-
-  // let text = 'Waiting...';
-  // if(errorMsg) {
-  //   text = errorMsg;
-  // }
-  // else if (location) {
-  //   text = JSON.stringify(location);
-  // }
-  <View>
-    <StatusBar style="auto" />
-    <MapView style={styles.map} initialRegion={{
-      latitude: 29.643633,
-      longitude: -82.354927,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}/>
-  </View>
-  )
-}
-
-const Login = ({ navigation }) => {
-  const [email, setEmail] = useState(null);
-  const [pass, setPass] = useState(null);
-
-  const createNewUser = () => {
-    createUserWithEmailAndPassword(auth, email, pass)
-    .then((userCredential) => {
-      console.log("SUCCESSFUL SIGN-UP!");
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-    });
-  }
-
-  const loginUser = () => {
-    signInWithEmailAndPassword(auth, email, pass)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("SUCCESSFUL LOGIN!");
-      navigation.navigate("Map");
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage);
-    });
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style = {styles.baseText}>
-        <Text style = {styles.titleText}>
-          Locality
-        </Text>
-      </Text>
-      <TextInput
-        placeholder='E-mail'
-        autoCorrect={false}
-        style={styles.textInp}
-        keyboardType='email-address'
-        onChangeText={text => setEmail(text)}
-        value={email}
-      />
-      <TextInput
-        placeholder='Password'
-        autoCorrect={false}
-        style={styles.textInp}
-        secureTextEntry={true}
-        onChangeText={text => setPass(text)}
-        value={pass}
-      />
-      <View style={{
-        justifyContent:'space-evenly',
-        flexDirection: 'row',
-        marginTop: 20
-      }}>
-        <TouchableOpacity
-          onPress={() => loginUser()}
-          style={styles.button}>
-            <Text style={styles.buttonText}> Log-in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => createNewUser()}
-          style={styles.button}>
-            <Text style={styles.buttonText}> Sign-up</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='LoginScreen'>
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Map' component={Map} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-  baseText: {
-    fontFamily: "Times New Roman"
-  },
-  titleText: {
-    fontFamily: "Helvetica",
-    fontSize: 50,
-    color: '#382',
-  },
-  button: {
-    backgroundColor: '#382',
-    padding: 15,
-    borderRadius: 5,
-    marginHorizontal: 10,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: 'white'
-  },
-  textInp: {
-    borderColor: '#666',
-    borderWidth: 1,
-    borderRadius: 3,
-    marginTop: 10,
-    width: 200
-  },
-});
-
-
-var localityData = [
+const localityData = [
   {
     "Address": "5001 NW 34th Blvd, Gainesville, FL 32605",
     "Name": "African Unique - International Marketplace",
@@ -488,6 +327,187 @@ var localityData = [
     "Lat": "29.6302567",
     "Long": "-82.3388575"
   }
-];
+]
+
+state = {localityData};
+
+mapMarkers = () => {
+  return this.state.localityData.map((report) => <Marker
+    key={report.Address}
+    coordinate={{ latitude: report.Lat, longitude: report.Long }}
+    title={report.Name}
+    description={report.Type}
+  >
+  </Marker >)
+}
+
+const Map = () => {
+  return(
+  // const [location, setLocation]  = useState(null);
+  // const [errorMsg, setErrorMsg] = useState(null);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     let {status} = await Location.requestBackgroundPermissionsAsync();
+  //     if(status !== 'granted') {
+  //       setErrorMsg('location permissoins denied')
+  //       return;
+  //     }
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     setLocation(location);
+  //   })();
+  // }, []);
+
+  // let text = 'Waiting...';
+  // if(errorMsg) {
+  //   text = errorMsg;
+  // }
+  // else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+  <View>
+    <StatusBar style="auto" />
+    <MapView 
+      style={styles.map} 
+        initialRegion={{
+        latitude: 29.643633,
+        longitude: -82.354927,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+    {this.mapMarkers()}
+    </MapView>  
+  </View>
+  )
+}
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState(null);
+  const [pass, setPass] = useState(null);
+
+  const createNewUser = () => {
+    createUserWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+      console.log("SUCCESSFUL SIGN-UP!");
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  }
+
+  const loginUser = () => {
+    signInWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("SUCCESSFUL LOGIN!");
+      navigation.navigate("Map");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.baseText}>
+        <Text style = {styles.titleText}>
+          Locality
+        </Text>
+      </Text>
+      <TextInput
+        placeholder='E-mail'
+        autoCorrect={false}
+        style={styles.textInp}
+        keyboardType='email-address'
+        onChangeText={text => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        placeholder='Password'
+        autoCorrect={false}
+        style={styles.textInp}
+        secureTextEntry={true}
+        onChangeText={text => setPass(text)}
+        value={pass}
+      />
+      <View style={{
+        justifyContent:'space-evenly',
+        flexDirection: 'row',
+        marginTop: 20
+      }}>
+        <TouchableOpacity
+          onPress={() => loginUser()}
+          style={styles.button}>
+            <Text style={styles.buttonText}> Log-in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => createNewUser()}
+          style={styles.button}>
+            <Text style={styles.buttonText}> Sign-up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LoginScreen'>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Map' component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  baseText: {
+    fontFamily: "Times New Roman"
+  },
+  titleText: {
+    fontFamily: "Helvetica",
+    fontSize: 50,
+    color: '#382',
+  },
+  button: {
+    backgroundColor: '#382',
+    padding: 15,
+    borderRadius: 5,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white'
+  },
+  textInp: {
+    borderColor: '#666',
+    borderWidth: 1,
+    borderRadius: 3,
+    marginTop: 10,
+    width: 200
+  },
+});
+
+
+
 
 export default App;
